0c10cbca16c90b22c40d7595af8ee86caf6aa25a
Anna Sanderson <anna.sanderson@csuglobal.edu>
2016-06-07 17:35:33 -0400

    Put some for now finishing touches onto my css, html. Started planning the js

---
+++ app.js
@@ -0,0 +1,12 @@
+//Planning phase:
+//Should create an onload function
+//Should declare a variable for an array for all cards. Will download images of cards.
+//Should generate cards by grabbing element by the id when the button is clicked. Should loop over the array to shuffle to cards.
+//The cards should fall randomly onto the page.
+//Cards should be clickable and should go into a pile *or* can disappear from the table
+//When the button is clicked, the timer should start AND the cards will be thrown onto the table.
+
+//TL/DR: When the button is clicked the following should happen:
+	//Cards are generated and thrown randomly onto the page
+	//The timer starts counting download
+	
